937104fd0f5044151539740ac8b3f761224ff9ef
lucaslllll <lucascosta12367@gmail.com>
2019-07-20 13:52:07 -0300

    Coloquei 3 models

--- Delivery/models.py
+++ Delivery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from internationalphone.models import InternationalPhoneNumberField
 
-# Create your models here.
+
+class Conta(models.Model):
+	usuario = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+	cpf = models.NumberField(max_length=11, help_text="Digite seu CPF sem  qualquer ponto ou hífen")
+	localizacao = models.CharField(max_length=500)
+	telefone = models.InternationalPhoneNumberField(default_region='BR')
+
+
+
+class Restaurante(models.Model):
+	nome = models.CharField(max_length=500)
+	dono = models.CharField(max_length=500)
+	localizacao = models.CharField(max_length=500)
+	telefone = models.CharField()
+
+
+class Comentario(models.Model):
+	autor = models.OneToOneField(Conta, on_delete=models.CASCADE, blank=True, null=True)
+	titulo = models.CharField(max_length=100)
+	descricao = models.CharField(max_length=1000)
