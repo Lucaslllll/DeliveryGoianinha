9bacc0bd3ab92647e33b6d9e3f725b2949eff3f4
Lucaslllll <lucascosta12367@gmail.com>
2019-09-07 14:03:36 -0300

    CRUD

--- API/Delivery/urls.py
+++ API/Delivery/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^conta/$', views.ContaList.as_view(), name='conta-list'),
-
+    url(r'^usuario_lista/$', views.UsuarioLista.as_view(), name='usuario_lista'),
+    url(r'^usuario/(?P<pk>\d+)$', views.UsuarioDetalhes.as_view(), name='usuario'),
 ]
--- API/Delivery/views.py
+++ API/Delivery/views.py
@@ -3,7 +3,10 @@ from .models import Usuario, Restaurante, Pedido, Pedido_Restaurante, Comentario
 from .serializers import UsuarioSerializer, RestauranteSerializer, PedidoSerializer, PedidoRestauranteSerializer, ComentarioSerializer
 
 # Create your views here.
-class ContaList(generics.ListCreateAPIView):
+class UsuarioLista(generics.ListCreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
 
+class UsuarioDetalhes(generics.RetrieveUpdateDestroyAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
